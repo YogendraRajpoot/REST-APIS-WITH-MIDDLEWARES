9545273844d0a61e25593b3c91e115665bcd0246
YogendraRajpoot <yogendrarajpooten1030@gmail.com>
2022-04-20 06:15:50 +0530

    Completed

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Welcome to home page");
+});
+app.get("/books", (req, res) => {
+  res.send("Welcome to Book's page");
+});
+app.get("/books/:name", (req, res) => {
+  res.send({ bookName: req.params.name });
+});
+
+module.exports = app;
