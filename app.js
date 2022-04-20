f92d683e1540c50cd92a80e959db33a8f67a74b5
YogendraRajpoot <yogendrarajpooten1030@gmail.com>
2022-04-20 06:49:52 +0530

    update bugs

--- app.js
+++ app.js
@@ -4,9 +4,16 @@ const app = express();
 app.get("/", (req, res) => {
   res.send("Welcome to home page");
 });
-app.get("/books", (req, res) => {
-  res.send("Welcome to Book's page");
-});
+app.get(
+  "/books",
+  (req, res, next) => {
+    console.log("Fetching all books");
+    next();
+  },
+  (req, res) => {
+    res.send("Welcome to Book's page");
+  }
+);
 app.get("/books/:name", (req, res) => {
   res.send({ bookName: req.params.name });
 });
